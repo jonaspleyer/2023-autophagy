d535a3bef29201c36316cf72e43bfad891582b72
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2023-12-19 00:27:12 +0100

    sphinx documentation first try

---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.append(os.path.abspath('../../'))
+import cr_autophagy
+
+project = 'cr_autophagy'
+copyright = '2023, Jonas Pleyer'
+author = 'Jonas Pleyer'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+]
+
+autosummary_generate = True
+add_module_names = False
+autodoc_typehints_format = 'short'
+
+templates_path = ['_templates']
+exclude_patterns = ["out", "data"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,27 @@
+.. cr_autophagy documentation master file, created by
+   sphinx-quickstart on Mon Dec 18 21:02:19 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+``cellular_raza`` Autophagy
+========================================
+
+This is a very short summary of all the so far produced functionality surrounding cr_autophagy
+package.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   Analysis <analysis>
+   Plotting <plotting>
+   Parameter Space <param_space>
+   Rust Bindings <rust-bindings>
+   Storage <storage>
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
