2c91dd1225a1ceea362182679a6a697d2da5559e
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-06-11 13:16:33 +0200

    new default values for simulation (mainly commented out)
    - see also previous commits
    - also by default open most recent sim in firefox

--- script_run_sim.py
+++ script_run_sim.py
@@ -6,27 +6,37 @@ if __name__ == "__main__":
     simulation_settings = SimulationSettings()
     simulation_settings.n_threads = 4
     simulation_settings.t_max = 4 * cra.MINUTE
-    simulation_settings.save_interval = 0.1 * cra.MINUTE
-    simulation_settings.dt = 0.001 * cra.MINUTE
+    # simulation_settings.save_interval = 0.01 * cra.MINUTE
+    # simulation_settings.dt = 0.00025 * cra.MINUTE
 
-    factor = 2e-2 * cra.NANOMETRE**2 / cra.SECOND**2
-    simulation_settings.potential_strength_cargo_cargo          = factor * 0.03
-    simulation_settings.potential_strength_cargo_atg11w19       = factor * 0.01
-    simulation_settings.potential_strength_atg11w19_atg11w19    = factor * 0.005
+    units = cra.MICROMETRE**2 / cra.SECOND**2
+    # simulation_settings.potential_strength_cargo_cargo          = units * 3e0
+    simulation_settings.potential_strength_cargo_atg11w19       = units * 4e-1
+    # simulation_settings.potential_strength_atg11w19_atg11w19    = units * 5e-1
 
-    simulation_settings.domain_size = 2000 * cra.NANOMETRE
-    simulation_settings.domain_cargo_radius_max = 450 * cra.NANOMETRE
-    # simulation_settings.domain_atg11w19_radius_min = 550 * cra.NANOMETRE
+    # High avidity
+    simulation_settings.potential_strength_atg11w19_atg11w19    = units * 2e-1
 
-    simulation_settings.diffusion_atg11w19 = 5e-4  * cra.MICROMETRE**2 / cra.SECOND
-    simulation_settings.diffusion_cargo = 2e-4  * cra.MICROMETRE**2 / cra.SECOND
+    # simulation_settings.domain_size = 2500 * cra.NANOMETRE
+    # simulation_settings.domain_cargo_radius_max = 450 * cra.NANOMETRE
+    # # simulation_settings.domain_atg11w19_radius_min = 550 * cra.NANOMETRE
+    # simulation_settings.domain_n_voxels = 5
 
-    # storager = run_simulation(simulation_settings)
+    # simulation_settings.diffusion_cargo = 5e-5  * cra.MICROMETRE**2 / cra.SECOND
+    # simulation_settings.diffusion_atg11w19 = 8e-5 * cra.MICROMETRE**2 / cra.SECOND
+
+    # simulation_settings.interaction_range_atg11w19_cargo *= 2.0
+
+    # simulation_settings.n_cells_cargo = 220
+    # simulation_settings.n_cells_atg11w19 = 180
+
+    storager = run_simulation(simulation_settings)
 
     output_path = cra.get_last_output_path()
 
     print("Saving Snapshots")
-    cra.save_all_snapshots(output_path, threads=14, overwrite=True)#simulation_settings.n_threads)
+    cra.save_all_snapshots(output_path, threads=14, overwrite=True, transparent_background=True)
+    #simulation_settings.n_threads)
     # cra.save_snapshot(output_path, 100, overwrite=True)
 
     # print("Saving cluster analysis plots")
@@ -58,3 +68,6 @@ if __name__ == "__main__":
         -pix_fmt yuv420p\
         -strict -2 {output_path}/snapshot_movie.mp4"
     os.system(bashcmd)
+
+    bashcmd2 = f"firefox {output_path}/snapshot_movie.mp4"
+    os.system(bashcmd2)
