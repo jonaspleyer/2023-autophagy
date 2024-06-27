6902e4fd4df9e10365b842c6a96a39e50da2bf69
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-06-27 15:38:58 +0200

    do not use logarithmic or linear scale but just a listing of all possible results

--- script_parameter_space_plot.py
+++ script_parameter_space_plot.py
@@ -36,7 +36,12 @@ if __name__ == "__main__":
     values_potential_strength_cargo_atg11w19 = units * np.array([1e-2, 4e-1, 16e0])
     values_potential_strength_atg11w19_atg11w19 = units * np.array([2e-1, 1e0, 5e0])
 
-    def _run_sim(n_run: int, pot_aa: float, pot_ac: float, n_threads:int=1) -> tuple[Path, SimulationSettings]:
+    def _run_sim(
+        n_run: int,
+        ny_pot_aa: tuple[int, float],
+        nx_pot_ac: tuple[int, float],
+        n_threads:int=1
+    ) -> tuple[int, int, Path, SimulationSettings]:
         simulation_settings = SimulationSettings()
         simulation_settings.storage_name = OUT_PATH
         simulation_settings.substitute_date = str("{:010}".format(n_run))
@@ -55,22 +60,25 @@ if __name__ == "__main__":
 
         simulation_settings.interaction_range_atg11w19_cargo *= 0.5
 
-        return generate_results(simulation_settings)
+        return (ny_pot_aa[0], nx_pot_ac[0], *generate_results(simulation_settings))
 
     n_threads = 2
     n_cores = mp.cpu_count()
     n_workers = max(1, math.floor(n_cores / n_threads))
 
-    def __run_sim_helper(args: list) -> tuple[Path, SimulationSettings]:
+    def __run_sim_helper(args: list) -> tuple[int, int, Path, SimulationSettings]:
         return _run_sim(*args)
 
-    n_prev_runs = max([int(str(Path(p)).split("/")[-1]) for p in glob.glob(str(OUT_PATH) + "/*")] + [0]) + 1
+    n_prev_runs = max([
+        int(str(Path(p)).split("/")[-1])
+        for p in glob.glob(str(OUT_PATH) + "/*")
+    ] + [0]) + 1
     values = list(map(lambda x: (x[0], *x[1], *x[2:]),
         zip(
             itertools.count(n_prev_runs),
             itertools.product(
-                values_potential_strength_atg11w19_atg11w19,
-                values_potential_strength_cargo_atg11w19
+                enumerate(values_potential_strength_atg11w19_atg11w19),
+                enumerate(values_potential_strength_cargo_atg11w19),
             ),
             itertools.repeat(n_threads),
     )))
@@ -80,42 +88,48 @@ if __name__ == "__main__":
     print("Get Results")
     results = list(tqdm.tqdm(pool.imap(__run_sim_helper, values), total=len(values)))
 
-    fig, ax = plt.subplots(figsize=(16, 12))
-    pot_ac_max = np.max([sims[1].potential_strength_cargo_atg11w19 for sims in results])
-    pot_ac_min = np.min([sims[1].potential_strength_cargo_atg11w19 for sims in results])
-    pot_aa_max = np.max([sims[1].potential_strength_atg11w19_atg11w19 for sims in results])
-    pot_aa_min = np.min([sims[1].potential_strength_atg11w19_atg11w19 for sims in results])
-    base = 10
-    dn_ac = np.log(pot_ac_max / pot_ac_min) / np.log(base) / len(values_potential_strength_cargo_atg11w19)
-    dn_aa = np.log(pot_aa_max / pot_aa_min) / np.log(base) / len(values_potential_strength_atg11w19_atg11w19)
-    ax.set_xlim([pot_ac_min * base**(-dn_ac), pot_ac_max * base**dn_ac])
-    ax.set_ylim([pot_aa_min * base**(-dn_aa), pot_aa_max * base**dn_aa])
-    ax.set_xscale("log")
-    ax.set_yscale("log")
+    figsize_x = 12
+    figsize_y = (1 + len(values_potential_strength_atg11w19_atg11w19))\
+        / (1 + len(values_potential_strength_cargo_atg11w19))\
+        * figsize_x
+    fig, ax = plt.subplots(figsize=(figsize_x, figsize_y))
+    ax.set_xlim([0, len(values_potential_strength_cargo_atg11w19)+1])
+    ax.set_ylim([0, len(values_potential_strength_atg11w19_atg11w19)+1])
 
     print("Create Plot")
-    for opath, sim_settings in tqdm.tqdm(results, total=len(results)):
+    for ny, nx, opath, sim_settings in tqdm.tqdm(results, total=len(results)):
         # Retrieve information and plot last iteration
         last_iter = np.sort(cra.get_all_iterations(opath))[-1]
         try:
-            arr_img = cra.save_snapshot(opath, last_iter, transparent_background=True, overwrite=True)
+            arr_img = cra.save_snapshot(
+                opath,
+                last_iter,
+                transparent_background=True,
+                overwrite=True
+            )
         except:
             print("Failed to plot results from {}".format(opath))
 
-        pot_ac = sim_settings.potential_strength_cargo_atg11w19
-        pot_aa = sim_settings.potential_strength_atg11w19_atg11w19
-
         # Plot the box of the result
-        img = OffsetImage(arr_img, zoom=0.35)
+        img = OffsetImage(arr_img, zoom=0.4)
         ab = AnnotationBbox(
             img,
-            (pot_ac, pot_aa),
+            (nx+1, ny+1),# (pot_ac, pot_aa),
             pad=0,
             box_alignment=(0.5, 0.5),
             annotation_clip=True,
             frameon=False,
         )
         ax.add_artist(ab)
+
+    ax.set_xticks(
+            range(1, 1+len(values_potential_strength_cargo_atg11w19)),
+        values_potential_strength_cargo_atg11w19
+    )
+    ax.set_yticks(
+        range(1, 1+len(values_potential_strength_atg11w19_atg11w19)),
+        values_potential_strength_atg11w19_atg11w19
+    )
 
     ax.set_xlabel("Potential Strength Cargo-Protein")
     ax.set_ylabel("Potential Strength Protein-Protein")
